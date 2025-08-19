e20ede60e25b00e9ebb9819fd0c2d9f2c095a5ad
Tanvirahmed <tanvirchy269@gmail.com>
2025-08-19 09:55:51 +0600

    Add Qt .gitignore to ignore build files

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ClientSide_FILE C:/Users/sdeve/Qt Projects/ClientSide/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/ClientSide.exe)
-set(__QT_DEPLOY_TARGET_ClientSide_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ClientSide_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/deploy_ClientSide_c6164f7ebc.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/sdeve/Qt Projects/ClientSide/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/ClientSide-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE C:/Users/sdeve/Qt Projects/ClientSide/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/ClientSide.exe
-    GENERATE_QT_CONF
-)
